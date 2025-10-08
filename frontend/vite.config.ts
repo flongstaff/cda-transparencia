509a2e99f50d5c73fbd25fca72721dc58f3eb923
Franco Longstaff <franco.longstaff@gmail.com>
2025-10-08 15:09:11 +0800

    Trigger pages-build-deployment workflow

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-// Trigger deployment after @deck.gl/widgets fix
+//// Trigger deployment after @deck.gl/widgets fix
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
